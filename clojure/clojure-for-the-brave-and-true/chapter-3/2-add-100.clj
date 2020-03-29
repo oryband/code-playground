ed100df2d00a263b3af06774c68a4f7ddbc00fb9
Ory Band <oryband@users.noreply.github.com>
2020-03-29 22:09:48 +0300

    add clojure brave chapter 3 exercise 2

---
+++ clojure/clojure-for-the-brave-and-true/chapter-3/2-add-100.clj
@@ -0,0 +1,10 @@
+(defn add-100
+  "Write a function that takes a number and adds 100 to it."
+  [num]
+  (+ 100 num))
+
+
+(do
+    (println (add-100 1))
+    (println (add-100 10))
+    (println (add-100 100)))
